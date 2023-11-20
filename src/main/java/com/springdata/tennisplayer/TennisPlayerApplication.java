61402bdbf7621cbbedb73fdc0cc95722bd2d80bb
abdessamadalami <eloualialamiabdessamad@gmail.com>
2023-11-20 08:27:40 +0100

    start

---
+++ src/main/java/com/springdata/tennisplayer/TennisPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.springdata.tennisplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisPlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springdata/tennisplayer/TennisPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdata.tennisplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
